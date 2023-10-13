99d5d90cddced9d36a7705260e95691a5a071009
Max Hirning <maxhirning25@gmail.com>
2023-10-13 15:01:22 +0300

    Added icons

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,16 @@
+import { join } from 'path';
+import * as express from 'express';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/assets', express.static(join(__dirname, 'assets')));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
+  
   console.log(`App is running on: ${process.env.PORT} port`);
 }
 
